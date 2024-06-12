391f138fbb77a1e6aa17715b73440cfce5300c1c
aganyo360 <wilsonaganyo@gmail.com>
2024-06-13 02:14:02 +0300

    updated db

--- blog/app/urls.py
+++ blog/app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+    path('posts/new/', views.post_new, name='post_new'),
+    path('posts/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/app/views.py
+++ blog/app/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth import login, authenticate
 from .forms import RegisterForm
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import logout
+from .models import Post
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+
+
 
 def register(request):
     if request.method == 'POST':
@@ -25,3 +30,38 @@ def logout_view(request):
     return redirect('home')
 def home(request):
     return render(request, 'app/home.html')
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'app/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'app/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'app/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'app/post_edit.html', {'form': form})
