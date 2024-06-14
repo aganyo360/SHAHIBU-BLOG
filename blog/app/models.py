23fbb153c416b12885f2915a5d8142424fbebda5
aganyo360 <wilsonaganyo@gmail.com>
2024-06-14 19:16:04 +0300

    created input fiels for post

--- blog/app/models.py
+++ blog/app/models.py
@@ -1,9 +1,6 @@
-from django.db import models
-
+# app/models.py
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-# app/models.py
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
